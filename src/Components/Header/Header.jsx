6200385044f6190a288c57a3c171fc941e92416c
Sasha Tribis <tribisalexandr@yandex.ru>
2020-09-21 21:50:03 +0300

    123

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-    <img src="https://avatars.mds.yandex.net/get-pdb/1613577/07cd296a-51bb-47a8-a3dd-115792dfa964/s1200" />
+    <img src="https://avatars.mds.yandex.net/get-pdb/1613577/07cd296a-51bb-47a8-a3dd-115792dfa964/s1200"/>
   </header>
 }
 
